28d6eb56c712d845aeed24cf5737a7bdc01dd08b
Erik Tollerud <erik.tollerud@gmail.com>
2020-12-08 15:50:01 -0500

    simplify server structure

--- tile_server.py
+++ tile_server.py
@@ -35,30 +35,24 @@ def load_fits(filename: str, hdunum: int):
         hdu = f[hdunum]
         ptp = np.ptp(hdu.data)
         dmin = np.min(hdu.data)
-        rescaled_data = (hdu.data-dmin)*255/ptp
+        rescaled_data = (hdu.data-dmin)/ptp # [0, 1]
 
         app.hashes[hsh] = rescaled_data
 
     return hsh
 
 
-@app.get("/tile/{hashstr}/{zoom}/{x}.{y}")
-def load_tile(hashstr:str, zoom: int, x: float, y: float):
-    hashtype = hashstr.split('_')[0]
-    if hashtype == 'fits':
-        hashsplit = hashstr.split('_')
-        return tile_fits(hashstr, zoom, x, y)
-    else:
-        raise NotImplementedError(f"hash type {hashtype} not yet supported")
-
-def tile_fits(hashstr: str, zoom: int, x: float, y: float):
+@app.get("/tile/{hashstr}/{zoom}/{x}.{y}.{format}")
+def load_tile(hashstr:str, zoom: int, x: float, y: float, format: str):
     hdu_data_scaled = app.hashes[hashstr]
 
     x = int(x*TILE_SIZE)
     y = int(y*TILE_SIZE)
 
-    tile_png = png.from_array(hdu_data_scaled[x:x+TILE_SIZE, y:y+TILE_SIZE].astype(np.uint8), mode="L")
-    bio = io.BytesIO()
-    tile_png.write(bio)
+    tile_data_array = hdu_data_scaled[x:x+TILE_SIZE, y:y+TILE_SIZE]
 
-    return Response(content=bio.getvalue(), media_type='image/png')
+    if format == 'png':
+        bio = io.BytesIO()
+        tile_png = png.from_array((tile_data_array*255).astype(np.uint8), mode="L")
+        tile_png.write(bio)
+        return Response(content=bio.getvalue(), media_type='image/png')
