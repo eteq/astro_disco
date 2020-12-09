fdef9539d5984ba76f01a95666156cbf5fbaae5f
Erik Tollerud <erik.tollerud@gmail.com>
2020-12-08 22:53:33 -0500

    still off by a bit but oriented right

--- tile_server.py
+++ tile_server.py
@@ -1,11 +1,17 @@
+import os
+import io
+import secrets
+
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import Response
 
-import os
-import io
 import png
+
 import numpy as np
+
+from astropy import visualization
 from astropy.io import fits
+
 
 app = FastAPI()
 
@@ -14,6 +20,7 @@ app.images = {}
 app.hashes = {}
 
 TILE_SIZE = 256
+TOKEN_SIZE = 5
 
 
 @app.get("/loadfits/{filename}/{hdunum}")
@@ -27,29 +34,27 @@ def load_fits(filename: str, hdunum: int):
     if (hdunum+1) > len(app.images[filename]):
         raise HTTPException(status_code=436, detail=f"HDU {hdunum} not found")
 
-    hsh  = f"fits_{filename}_{hdunum}_1"
+    hsh  = f"fits_{filename}_{hdunum}_" + secrets.token_hex(TOKEN_SIZE)
 
     if hsh not in app.hashes:
         f = app.images[filename]
 
         hdu = f[hdunum]
-        ptp = np.ptp(hdu.data)
-        dmin = np.min(hdu.data)
-        rescaled_data = (hdu.data-dmin)/ptp # [0, 1]
 
-        app.hashes[hsh] = rescaled_data
+        siv = visualization.LogStretch() + visualization.AsymmetricPercentileInterval(15, 99.5)
+        app.hashes[hsh] = siv(hdu.data) # scales to [0, 1]
 
     return hsh
 
 
-@app.get("/tile/{hashstr}/{zoom}/{x}.{y}.{format}")
+@app.get("/tile/{hashstr}/{zoom}/{x}/{y}.{format}")
 def load_tile(hashstr:str, zoom: int, x: float, y: float, format: str):
     hdu_data_scaled = app.hashes[hashstr]
 
     x = int(x*TILE_SIZE)
-    y = int(y*TILE_SIZE)
+    y = int(-y*TILE_SIZE)
 
-    tile_data_array = hdu_data_scaled[x:x+TILE_SIZE, y:y+TILE_SIZE]
+    tile_data_array = hdu_data_scaled[(y+TILE_SIZE):y:-1, x:x+TILE_SIZE]
 
     if format == 'png':
         bio = io.BytesIO()
